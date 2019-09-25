927d94de343f327b16f295b600c31c7dbbf31636
Keisuke Mori <rjfs29kei@gmail.com>
2019-09-25 10:16:33 +0900

    add set_current_time to tweet feature

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,10 @@
 class Tweet < ApplicationRecord
+  before_save :set_tweeted_time, if: :new_record?
   validates :message, presence: true, length: { in: 1..140 }
+
+  private
+
+  def set_tweeted_time
+    self.tdate = Time.current
+  end
 end
